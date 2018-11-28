03527cfe3dfdc87b7c218c5774c71399cc5d50fb
Kristian Mandarić <mandaric.kristian@gmail.com>
2018-11-28 18:36:41 +0100

    Find Crosshair at screen location

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -38,7 +38,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	FVector HitLocation; //Out parameter
 	if(GetSightRayHitLocation(HitLocation)) // Has "side effect", is going to line trace.
 	{
-		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		// UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString());
 
 	  // TODO tell controlled tank to aim at this point
 	}
@@ -48,6 +48,13 @@ void ATankPlayerController::AimTowardsCrosshair()
 //Get world location if linetrace through crosshair, true if hits landscape
 bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation)const
 {
-	HitLocation = FVector(1.0);
+	// Find the crosshair position
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
+	UE_LOG(LogTemp, Warning, TEXT("ScreeenLocation: %s"), *ScreenLocation.ToString());
+	// "De-project" the screen position of the crosshair to a world direction
+	// Line-trace along that look direction, see what we hit up to a maximum range.
+
 	return true;
 }
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -15,7 +15,7 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:	
+private:
 	ATank* GetControlledTank() const; 
 	
 	virtual void BeginPlay() override; 
@@ -24,6 +24,12 @@ public:
 
 	void AimTowardsCrosshair(); // Start the tank moving the barrel so that a shot would hit 
 								// where the crosshair intersects the world
-private:
+
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+	
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.3333;
 };
