3cfb05d961ae82b673cacfb8d09ca3c97e24cda7
Kristian Mandarić <mandaric.kristian@gmail.com>
2018-11-30 18:46:26 +0100

    Added Firing Speed category for Projectile Velocity

--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -32,6 +32,9 @@ private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditAnywhere, Category = Firing)
+		float LaunchSpeed = 100000; // Sensible starting value of 1000m/s
+
 protected:
 	UTankAimingComponent* TankAimingComponent = nullptr;
 	
